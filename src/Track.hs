5f0b653126c2020b8074b4732dabbad86a607ec8
Andrey Sverdlichenko <blaze@ruddy.ru>
2012-02-15 13:19:23 +0400

    MinQueue not needed and removed. Implemented Vincenty's formulae for finding distance between points.

--- src/Track.hs
+++ src/Track.hs
@@ -1,9 +1,49 @@
-module Track (Position(Position), TrackPoint(TrackPoint)) where
+module Track (Position(Position), TrackPoint(TrackPoint), vincentyDistance) where
 
+import Data.Ratio ((%))
 import Data.Time (UTCTime)
+import Math.Sequence.Converge (convergeTo)
 
 data Position = Position Double Double  -- Lat Long
     deriving (Show)
 data TrackPoint = TrackPoint UTCTime Position Double -- Altitude
     deriving (Show)
-    
+
+vincentyDistance :: Position -> Position -> Double
+vincentyDistance pos1 pos2 = round2mm (b * al * (fin_sig - dsig))
+    where
+    p2 = 2 :: Int
+    a = 6378137.0       -- length of major axis of the ellipsoid (radius at equator)
+    f = 1/298.257223563 -- flattening of the ellipsoid
+    b = (1 - f) * a      -- length of minor axis of the ellipsoid (radius at the poles)
+    Position lat1 long1 = pos1
+    Position lat2 long2 = pos2
+    u1 = atan ((1 - f) * tan lat1)
+    u2 = atan ((1 - f) * tan lat2)
+    ll = long2 - long1
+    la_good = convergeTo 1e-12 0 (iterate stepla ll)
+    (fin_cos2al, fin_sinsig, fin_cossig, fin_cossigm2, _, fin_sig) = precalc la_good 
+    up2 = fin_cos2al * (a ^ p2 - b ^ p2) / (b ^ p2)
+    al = 1 + up2 / 16384 * (4096 + up2 * (-768 + up2 * (320 - 175 * up2)))
+    bl = up2 / 1024 * (256 + up2 * (-128 + up2 * (74 - 47 * up2)))
+    dsig = bl * fin_sinsig * (fin_cossigm2 + bl / 4 * (fin_cossig * (-1 + 2 * (fin_cossigm2 ^ p2)) - bl / 6 * fin_cossigm2 * (-3 + 4 * (fin_sinsig ^ p2) * (-3 + 4 * (fin_cossigm2 ^ p2)))))
+
+    precalc :: Double -> (Double, Double, Double, Double, Double, Double)
+    precalc la = (cos2al, sinsig, cossig, cossigm2, sinal, sig)
+        where
+        sinsig = sqrt ((cos u2 * sin la) ^ p2 + (cos u1 * sin u2 - sin u1 * cos u2 * cos la) ^ p2)
+        cossig = sin u1 * sin u2 + cos u1 * cos u2 * cos la
+        sig = atan2 sinsig cossig
+        sinal = cos u1 * cos u2 * sin la / sinsig
+        cos2al = 1 - (sinal ^ p2)
+        cossigm2 = cossig - 2 * sin u1 * sin u2 / cos2al
+
+    stepla :: Double -> Double
+    stepla la = nla
+        where
+        (cos2al, sinsig, cossig, cossigm2, sinal, sig) = precalc la 
+        cl = f / 16 * cos2al * (4 + f * (4 - 3 * cos2al))
+        nla = ll + (1 - cl) * f * sinal * (sig + cl * sinsig * (cossigm2 + cl * cossig * (-1 + 2 * (cossigm2 ^ p2))))
+
+    round2mm :: Double -> Double
+    round2mm x = fromRational (round (x * 1000) % 1000)    
--- tests/Main.hs
+++ tests/Main.hs
@@ -2,68 +2,24 @@ module Main where
 
 import Test.Framework(defaultMain, testGroup)
 import Test.Framework.Providers.HUnit
-import Test.Framework.Providers.QuickCheck2
+-- import Test.Framework.Providers.QuickCheck2
 import Test.HUnit
 -- import Test.QuickCheck
-import qualified MinQueue
 
-import Data.Maybe
-import Debug.Trace
+import Track
 
 main :: IO()
 main = defaultMain tests
-  where tests = [
-            testGroup "MinQueue" [
-                testCase "Empty Queue" testEmptyQueue,
-                testCase "Emptied Queue" testEmptiedQueue,
-                testProperty "LIFO" testLIFO,
-                testProperty "Short LIFO" testShortLIFO,
-                testProperty "Minimum" testMin
-            ]
-         ]
+    where 
+    tests = [
+        testGroup "Vincenty" [
+            testCase "FlindersPeak - Buninyong" testVinc1
+        ]
+     ]
 
-type QState = (MinQueue.MinQueue Int, [Int])
-
-stateMove :: QState -> QState
-stateMove (q, vals) = ( MinQueue.pop q, vals ++ [fromJust $ MinQueue.peek q])
-
-stateEmpty :: QState -> Bool
-stateEmpty (q, _) = MinQueue.null q
-
-testEmptyQueue :: Assertion
-testEmptyQueue = isNothing v @?= True
+testVinc1 :: Assertion    
+testVinc1 = vincentyDistance flindersPeak buninyong @?= 54972.271
     where
-        v = MinQueue.peek (MinQueue.empty :: MinQueue.MinQueue Int) 
-
-testEmptiedQueue :: Assertion
-testEmptiedQueue = isNothing v @?= True
-    where
-        v = MinQueue.peek emptyq
-        emptyq = MinQueue.pop $ MinQueue.pop dataq
-        dataq = MinQueue.push 1 $ MinQueue.push 2 (MinQueue.empty :: MinQueue.MinQueue Int)
-
-testLIFO :: [Int] -> Bool
-testLIFO xs = xs == popped
-    where
-        queue = foldl (flip MinQueue.push) MinQueue.empty xs
-        popped = snd $ until stateEmpty stateMove (queue, [])
-
-testShortLIFO :: Int -> [Int] -> Bool
-testShortLIFO len xs = xs == popped
-    where
-        realLen = abs len `mod` (1 + length xs)
-        (prexs, lastxs) = splitAt realLen xs
-        preq = foldl (flip MinQueue.push) MinQueue.empty prexs
-        pushpop :: QState -> Int -> QState
-        pushpop (queue, out) v = (queue', out')
-            where
-                tq = MinQueue.push v queue
-                out' = out ++ [fromJust $ MinQueue.peek tq]
-                queue' = MinQueue.pop tq
-        state = foldl pushpop (preq, []) lastxs
-        popped = snd $ until stateEmpty stateMove state
-        
-testMin :: [Int] -> Bool
-testMin xs = (null xs && isNothing (MinQueue.minimum queue)) || fromJust (MinQueue.minimum queue) == minimum xs
-    where
-        queue = foldl (flip MinQueue.push) MinQueue.empty xs
+    g2r x = x * pi / 180
+    flindersPeak = Position (g2r $ -37.95103341666666666666) (g2r 144.42486788888888888888)
+    buninyong = Position (g2r $ -37.65282113888888888888) (g2r 143.92649552777777777777)
